388a83b605da630c112ca24058c5387f4d67b2bd
stoyko-yuri <stoyko.yuri@gmail.com>
2022-06-24 19:31:54 +0300

    change wait button, code optimization

--- src/app/components/stopwatch/stopwatch.component.ts
+++ src/app/components/stopwatch/stopwatch.component.ts
@@ -1,66 +1,75 @@
-import { Component, OnInit } from '@angular/core';
-import { timer } from 'rxjs';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
+import { buffer, debounceTime, interval, Observable, timer, map, fromEvent, filter } from 'rxjs';
 
 @Component({
   selector: 'app-stopwatch',
   templateUrl: './stopwatch.component.html',
   styleUrls: ['./stopwatch.component.css']
 })
-export class StopwatchComponent implements OnInit {
+export class StopwatchComponent implements OnInit, AfterViewInit {
   minutes = 0;
   seconds = 0;
   isRunning = false;
-  timer = timer(0, 1000);
+  stopwatchTimer = timer(0, 1000);
   subscription: any;
-  lastTime = "00:00"
-  clickCounter = 0;
+  lastTime = "00:00";
+  click$ = new Observable<Event>;
 
-  constructor() { }
-
-  start(): void {
-    this.isRunning = !this.isRunning;
-    
-    if (this.isRunning) {
-      this.subscription = this.timer.subscribe(val => {
-        this.seconds++;
-        if (this.seconds > 59) {
-          this.seconds = 0;
-          this.minutes++;
-        }
-      })
-    } else {
-      this.subscription.unsubscribe();
-      this.lastTime = this.getTime();
-      this.seconds = 0;
-      this.minutes = 0;
-    }
+  constructor() {
   }
 
-  wait(): void {
-    this.clickCounter++;
-    setTimeout(() => {
-      if (this.clickCounter === 2 && this.isRunning) {
-        console.log(this.getTime())
-        this.isRunning = false;
-        this.subscription.unsubscribe();
-      }
-      this.clickCounter = 0;
-    }, 500);
-  }
-
-  reset(): void {
-    this.seconds = 0;
-    this.minutes = 0;
-    this.subscription.unsubscribe();
-    this.isRunning = false;
-    this.start();
-  }
-
-  getTime(): string {
-    return `${this.minutes < 10 ? "0" + this.minutes : this.minutes}:${this.seconds < 10 ? "0" + this.seconds : this.seconds}`;
+  ngAfterViewInit(): void {
+    this.click$ = fromEvent(document.getElementById("ButtonWait")!, "click")
+    this.wait();
   }
 
   ngOnInit(): void {
+  }
+
+  toggleStopwatch(): void {
+    this.isRunning = !this.isRunning;
+    this.isRunning ? this.start() : this.stop();
+  }
+
+  start(): void {
+    this.subscription = this.stopwatchTimer.subscribe(() => {
+      this.seconds++;
+      if (this.seconds > 59) {
+        this.seconds = 0;
+        this.minutes++;
+      }
+    })
+  }
+
+  stop(): void {
+    this.lastTime = this.getTime();
+    this.clear();
+  }
+
+  wait(): void {
+    this.click$.pipe(buffer(this.click$.pipe(debounceTime(500))), filter(list => list.length > 1)).subscribe(() => {
+      this.isRunning = false;
+      this.subscription.unsubscribe();
+    })
+  }
+
+  reset(): void {
+    this.clear();
+    this.isRunning = true;
+    this.start();
+  }
+
+  clear(): void {
+    this.subscription.unsubscribe();
+    this.seconds = 0;
+    this.minutes = 0;
+  }
+
+  getTime(): string {
+    const minutes = this.minutes < 10 ? "0" + this.minutes : this.minutes;
+    const seconds = this.seconds < 10 ? "0" + this.seconds : this.seconds;
+
+    return `${minutes}:${seconds}`;
   }
 
 }
